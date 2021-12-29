37a7a653c12a7aedcb9d9f93cf39a6db8bf82284
francesco.giardini <giardini.fr@gmail.com>
2021-12-29 20:25:21 +0100

    - st_analysis_on_all_samples.py -> now you can choose if perform st_analysis, disarray, or both.

--- source/st_analysis_on_all_samples.py
+++ source/st_analysis_on_all_samples.py
@@ -25,17 +25,19 @@ def main(parser):
     args               = parser.parse_args()
     source_path        = args.source_path[0]
     _disarray_analysis = args.disarray
+    _skip_st_analysis  = args.skip_st_analysis
 
     # list of subfolders
     samples = os.listdir(source_path)
     samples.sort()
 
-    print(Bcolors.WARNING + '\n\n**************** Start Disarray Analysis on all Samples in :' + Bcolors.ENDC)
+    print(Bcolors.WARNING + '\n\n**************** Start Fibers Analysis on all Samples in :' + Bcolors.ENDC)
     print('- source_path : ', source_path)
     print('- list of samples:')
     for s in samples: 
         print('   -', s)
     print('- [optional] Perform Disarray analysis: ', _disarray_analysis)
+    print('- [optional] Skip Structure Tensor Analysis: ', _skip_st_analysis)
     print()
 
     # run analisys on each sample
@@ -45,34 +47,34 @@ def main(parser):
         # sample path
         spath = os.path.join(source_path, s)
 
-        # tiff file here
-        tiff_fnames = [t for t in os.listdir(spath) if t.endswith('.tif') or t.endswith('.tiff')]
-        
-        # parameters file here
+        # parameters file on the current path
         par_fnames = [p for p in os.listdir(spath) if p.startswith('parameters')]
-        
-        # check if there is only one sample and one param file in the current directory
-        if (len(tiff_fnames), len(par_fnames)) == (1, 1):
-            
-            # create path
+
+        if not _skip_st_analysis:
+
+            # tiff file here
+            tiff_fnames = [t for t in os.listdir(spath) if t.endswith('.tif') or t.endswith('.tiff')]
             tiffpath = os.path.join(spath, tiff_fnames[0])
 
-            # perform analsys calling the sub-script
-            os.system('python3 st_analysis.py -s {} -p {}'.format(tiffpath, par_fnames[0]))
-            # subprocess.call(['secondary.py', '-sf', mask_path])
+            # check if there is only one sample and one param file in the current directory
+            if (len(tiff_fnames), len(par_fnames)) == (1, 1):
 
-            if _disarray_analysis is True:
+                # perform analsys calling the sub-script
+                os.system('python st_analysis.py -s {} -p {}'.format(tiffpath, par_fnames[0]))
+                # subprocess.call(['secondary.py', '-sf', mask_path])
 
-                # search file numpy R in the current path
-                R_fnames = [r for r in os.listdir(spath) if r.startswith('R_') and r.endswith('.npy')]
+        if _disarray_analysis is True:
 
-                # check if there is only one R
-                if len(R_fnames) == 1:
-                    R_path = os.path.join(spath, R_fnames[0])
+            # search file numpy R in the current path
+            R_fnames = [r for r in os.listdir(spath) if r.startswith('R_') and r.endswith('.npy')]
 
-                    # perform disarray analsys calling the sub-script
-                    os.system('python3 local_disarray_by_R.py -r {} -p {}'.format(R_path, par_fnames[0]))
-                    # subprocess.call(['secondary.py', '-sf', mask_path])
+            # check if there is only one R
+            if len(R_fnames) == 1:
+                R_path = os.path.join(spath, R_fnames[0])
+
+                # perform disarray analsys calling the sub-script
+                os.system('python local_disarray_by_R.py -r {} -p {}'.format(R_path, par_fnames[0]))
+                # subprocess.call(['secondary.py', '-sf', mask_path])
 
     print(Bcolors.WARNING + '\n Finish to analyze all samples\n' + Bcolors.ENDC)
     return None
@@ -84,6 +86,8 @@ if __name__ == '__main__':
                         help='Perform st_analysis on all samples in input path.')
     parser.add_argument('-d', '--disarray', action='store_true', default=True, required=False,
                         help='if passed, perform disarray analysis on all samples after the st_analysis.py')
+    parser.add_argument('-x', '--skip-st-analysis', action='store_true', default=False, required=False,
+                        help='if passed, the script skip the st_analysis.')
 
     main(parser)
 
